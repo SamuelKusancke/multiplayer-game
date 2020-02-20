68501e33bd2c5e9da187b531748d2088e9c3bb12
Samuel Kusancke <58013012+SamuelKusancke@users.noreply.github.com>
2020-02-20 14:04:12 -0300

    Update server.js

--- server.js
+++ server.js
@@ -46,6 +46,6 @@ sockets.on('connection', (socket) => {
     })
 })
 
-server.listen(3000 || process.env.PORT, () => {
+server.listen(process.env.PORT || 5000, () => {
     console.log(`Server listening on port 3000`)
 })
